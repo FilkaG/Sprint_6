5918b66175a656499d0490480197a577a751a3ad
Georgy Filonenko <filonenko.zhorzh@yandex.ru>
2026-06-22 21:56:43 +0300

    second_try

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,61 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, _ = w.Write(content)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	result, err := service.Detect(string(data))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//outName := time.Now().UTC().String() + filepath.Ext(header.Filename)
+	outName := strings.ReplaceAll(time.Now().UTC().String(), ":", "-") + filepath.Ext(header.Filename)
+	err = os.WriteFile(outName, []byte(result), 0644)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(result))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Detect(data string) (string, error) {
+	if data == "" {
+		return "", errors.New("empty data")
+	}
+
+	trimmed := strings.TrimFunc(data, func(r rune) bool {
+		return r == '.' || r == '-' || unicode.IsSpace(r)
+	})
+
+	if trimmed == "" {
+		return morse.ToText(data), nil
+	}
+
+	return morse.ToMorse(data), nil
+}
